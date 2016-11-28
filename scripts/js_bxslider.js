782aaae90bdbf7a4e5e612dd1eeff6ac64df4d79
Viltas <vitalik_post@inbox.ru>
2016-11-28 23:31:37 +0300

    added dirs for new site

---
+++ scripts/js_bxslider.js
@@ -0,0 +1,7 @@
+<script>$(document).ready(function(){
+                  $('.bxslider').bxSlider({
+                  pager: false, // отключаем индикатор количества слайдов
+                  nextText: '', // отключаем текст кнопки Next
+                  prevText: '' // отключаем текст кнопки Prev
+                  });
+});</script>
